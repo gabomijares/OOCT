10c0ff8d7462f15ae2212176c51afe47d5d87d79
gabomijares <a01713970@tec.mx>
2024-11-23 00:52:14 -0600

    Update RaceTrack.h

--- RaceTrack.h
+++ RaceTrack.h
@@ -1,24 +1,22 @@
-#include <vector>
-
 #ifndef RACETRACK_H
 #define RACETRACK_H
 
 #include "Car.h"
+#include <vector>
+using namespace std;
 
 class RaceTrack {
 private:
     int track_length;
-    vector<Car*> cars;
+    vector<Car *> cars;
     string champion;
 
 public:
-    RaceTrack(int length);
-    ~RaceTrack();
-
-    void addCar(Car* car);
-    void greenFlag();
-    string getCarPositions() const;
-    string getChampion();
+    RaceTrack(int trackLength); 
+    void addCar(Car *car);     
+    void greenFlag();          
+    string getChampion() const; 
+    ~RaceTrack();               
 };
 
 #endif
