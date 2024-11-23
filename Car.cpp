1a9e2896edefa8148948729324d819f7744b4769
gabomijares <a01713970@tec.mx>
2024-11-23 00:47:14 -0600

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -1,18 +1,31 @@
 #include "Car.h"
 
-Car::Car(const string& team_name, int pit_time)
-    : team(team_name), position(0), pit_stop_time(pit_time) {}
-
-Car::~Car() {}
-
-int Car::getPosition() const {
-    return position;
-}
+Car::Car(string team, int pitStopTime) : team(team), position(0), pit_stop_time(pitStopTime) {}
 
 string Car::getTeam() const {
     return team;
 }
 
-void Car::pitStop() {
-    pit_stop_time--;
+void Car::setTeam(const string &team) {
+    this->team = team;
+}
+
+int Car::getPosition() const {
+    return position;
+}
+
+void Car::setPosition(int pos) {
+    position = pos;
+}
+
+int Car::getPitStopTime() const {
+    return pit_stop_time;
+}
+
+void Car::setPitStopTime(int time) {
+    pit_stop_time = time;
+}
+
+void Car::advance() {
+    position += rand() % 5 + 1; // Default random movement for testing
 }
