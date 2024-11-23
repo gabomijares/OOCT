7b08c65345169a2f44eb2dc83a46ebcceabe621e
gabomijares <a01713970@tec.mx>
2024-11-23 00:53:06 -0600

    Update RaceTrack.cpp

--- RaceTrack.cpp
+++ RaceTrack.cpp
@@ -1,43 +1,42 @@
 #include "RaceTrack.h"
 #include <iostream>
-using namespace std;
 
-RaceTrack::RaceTrack(int length) : track_length(length), champion("") {}
+RaceTrack::RaceTrack(int trackLength) : track_length(trackLength), champion("") {}
 
-RaceTrack::~RaceTrack() {
-    for (Car* car : cars) {
-        delete car;
-    }
-}
-
-void RaceTrack::addCar(Car* car) {
+void RaceTrack::addCar(Car *car) {
     cars.push_back(car);
 }
 
 void RaceTrack::greenFlag() {
-    bool race_finished = false;
+    bool raceOver = false;
 
-    while (!race_finished) {
-        for (Car* car : cars) {
+    while (!raceOver) {
+        for (Car *car : cars) {
             car->advance();
             if (car->getPosition() >= track_length) {
                 champion = car->getTeam();
-                race_finished = true;
+                raceOver = true;
                 break;
             }
         }
-        cout << getCarPositions() << endl;
+
+        // Display positions
+        for (Car *car : cars) {
+            cout << car->getTeam() << " Position: " << car->getPosition() << endl;
+        }
+        cout << "-------------------------" << endl;
+    }
+
+    cout << "The winner is: " << champion << "!" << endl;
+}
+
+string RaceTrack::getChampion() const {
+    return champion;
+}
+
+RaceTrack::~RaceTrack() {
+    for (Car *car : cars) {
+        delete car;
     }
 }
 
-string RaceTrack::getCarPositions() const {
-    string positions;
-    for (Car* car : cars) {
-        positions += car->getTeam() + ": " + to_string(car->getPosition()) + " units\n";
-    }
-    return positions;
-}
-
-string RaceTrack::getChampion() {
-    return "Champion: " + champion;
-}
